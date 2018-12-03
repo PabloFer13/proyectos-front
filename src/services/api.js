233db0d3de6d3e890486f76c32638c2791e8a841
Pablo Melendez <pablofer.melendez@gmail.com>
2018-12-03 16:02:36 -0500

    API route

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://proyectos-api.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:1337';
+axios.defaults.baseURL = 'https://proyectos-api.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:1337';
 
 const auth = token => {
   axios.defaults.params = { token };
